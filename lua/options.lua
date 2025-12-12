658b5c556e64d4070f4e4fab51866f7d50000457
Eddy <eddy_c@novellpharm.com>
2025-12-12 08:44:01 +0700

    Remove smartcase and set ignorecase to false

--- lua/options.lua
+++ lua/options.lua
@@ -32,8 +32,7 @@ vim.o.breakindent = true
 vim.o.undofile = true
 
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.o.ignorecase = true
-vim.o.smartcase = true
+vim.o.ignorecase = false
 
 -- Keep signcolumn on by default
 vim.o.signcolumn = 'yes'
