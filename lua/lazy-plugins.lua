d2d14b4995d70d6169f641276af69d40b7d05c5a
Eddy <eddy_c@novellpharm.com>
2026-05-28 18:20:40 +0700

    Add codex plugin

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -42,6 +42,7 @@ require('lazy').setup({
   require 'kickstart.plugins.vim-suda',
   require 'kickstart.plugins.vimtex',
   require 'kickstart.plugins.image',
+  require 'kickstart.plugins.codex',
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
