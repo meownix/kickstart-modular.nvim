f8cfeb234c8582cf4d7cdb64ec2d9c0976f445a0
Eddy <eddy_c@novellpharm.com>
2026-07-24 10:39:07 +0700

    Configure chromium app mode and theme toggle for peek.nvim
    Update peek.nvim configuration to use a custom chromium-app script.
    This allows zooming in the preview while hiding the browser UI elements.
    Add a new keymap <leader>tp to dynamically toggle the preview theme between
    light and dark by restarting the internal web server.

--- lua/keymaps/peek.lua
+++ lua/keymaps/peek.lua
@@ -2,4 +2,26 @@
 vim.keymap.set('n', '<leader>op', '<cmd>PeekOpen<CR>', { desc = '[O]pen Markdown [P]review (Peek)' })
 vim.keymap.set('n', '<leader>cp', '<cmd>PeekClose<CR>', { desc = '[C]lose Markdown [P]review (Peek)' })
 
+vim.keymap.set('n', '<leader>tp', function()
+  local peek = require('peek')
+  local current_theme = require('peek.config').get('theme')
+  local next_theme = current_theme == 'dark' and 'light' or 'dark'
+  
+  local is_open = peek.is_open()
+  if is_open then
+    peek.close()
+  end
+  
+  peek.setup({ theme = next_theme })
+  
+  -- If it was open, briefly wait for the old process to close then reopen
+  if is_open then
+    vim.defer_fn(function()
+      peek.open()
+    end, 100)
+  else
+    print('Peek theme set to: ' .. next_theme)
+  end
+end, { desc = '[T]oggle Markdown [P]review Theme (Peek)' })
+
 -- vim: ts=2 sts=2 sw=2 et
