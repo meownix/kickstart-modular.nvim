73a6692a1ab7896121f28791406a756e984ccc8c
Eddy <eddy_c@novellpharm.com>
2026-07-24 10:12:36 +0700

    Add peek.nvim for markdown preview
    Integrate peek.nvim to provide live preview of markdown files using deno and
    webview.
    Add vimwiki to the supported filetypes since it handles markdown buffers.
    Define keymaps <leader>op and <leader>cp for opening and closing the preview.

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -55,5 +55,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 require 'keymaps.vim-ai'
 require 'keymaps.vimwiki'
 require 'keymaps.fold-not-match'
+require 'keymaps.peek'
 
 -- vim: ts=2 sts=2 sw=2 et
---
+++ lua/keymaps/peek.lua
@@ -0,0 +1,5 @@
+-- Keymaps for peek.nvim (Markdown Preview)
+vim.keymap.set('n', '<leader>op', '<cmd>PeekOpen<CR>', { desc = '[O]pen Markdown [P]review (Peek)' })
+vim.keymap.set('n', '<leader>cp', '<cmd>PeekClose<CR>', { desc = '[C]lose Markdown [P]review (Peek)' })
+
+-- vim: ts=2 sts=2 sw=2 et
